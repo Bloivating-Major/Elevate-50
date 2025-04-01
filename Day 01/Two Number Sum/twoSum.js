26656cbf8ee2208c1bf7f2dd4ec443f16b6e3cda
Sambhav <sambhav12wakhariya@gmail.com>
2025-04-01 23:59:38 +0530

    Checked In : Two Sum Question

--- Day 01/Two Number Sum/twoSum.js
+++ Day 01/Two Number Sum/twoSum.js
@@ -14,7 +14,7 @@ var twoSum = function(nums, target) {
 // 2. Where we are checking for each and every element and adding it to check it is equal to our target or not
 
 
-// Better and Optimal Solution
+// Better  Solution
 let arr = [2,7,11,15], target = 9
 let map = new Map();
 for(let i = 0; i < arr.length; i++){
@@ -27,3 +27,35 @@ for(let i = 0; i < arr.length; i++){
 	map.set(arr[i], i); // Here we set element and index of element
 	// So that we can return it if we find it's sum.
 }
+
+// Optimal 
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    // Store original indices before sorting
+    let indexedNums = nums.map((num, index) => [num, index]);
+
+    // Sort based on values
+    indexedNums.sort((a, b) => a[0] - b[0]);
+
+    // 2 pointer approach
+    let left = 0;
+    let right = indexedNums.length - 1;
+
+    while (left < right) {
+        let currentSum = indexedNums[left][0] + indexedNums[right][0];
+
+        if (currentSum === target) {
+            return [indexedNums[left][1], indexedNums[right][1]]; // Return original indices
+        } else if (currentSum < target) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    
+    return [];
+};
